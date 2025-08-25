2e872ecdcca67799fcb3d27d2d32798edfde6b88
PihooGokhle <pihoogokhle@gmail.com>
2025-08-25 20:18:36 +0530

    add fibonacci.cpp file

---
+++ fibonacci.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int fibonacci(int n){
+    int a=0; 
+    int b=1; 
+    int c;
+    if(n <= 0) return 0;
+    if(n == 1) return 1;
+   
+
+    for(int i = 2; i <= n; i++){
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return b;
+}
+
+int main() {
+    int n;
+
+    cout << "Enter a no.: ";
+    cin >> n;
+
+    cout << fibonacci(n) << endl;
+
+    return 0;
+}
