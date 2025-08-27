584cf0508b3b6d553393fde0ea677c2bd654215c
PihooGokhle <pihoogokhle@gmail.com>
2025-08-27 20:37:57 +0530

    add for.cpp and frontBack.cpp file

---
+++ frontBack.cpp
@@ -0,0 +1,14 @@
+# include<iostream>
+# include<vector>
+using namespace std;
+
+int main() {
+
+vector<int> vec = {4, 5, 9, 7}; 
+
+cout << vec.front() << endl;
+
+cout << vec.back() << endl;
+
+return 0;
+}
