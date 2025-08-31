8d0fe01b0219f9466ed2797634cd9d3dc4bc61d4
PihooGokhle <pihoogokhle@gmail.com>
2025-08-31 22:59:26 +0530

    add pushBack.cpp fils

---
+++ pushBack.cpp
@@ -0,0 +1,23 @@
+# include<iostream>
+# include<vector>
+using namespace std;
+
+int main() {
+
+vector<int> vec; 
+
+cout << "Size: " << vec.size() << endl;
+
+vec.push_back(6);
+vec.push_back(12);
+vec.push_back(18);
+vec.push_back(24);
+
+cout << "Size: " << vec.size() << endl;
+
+for(int i: vec){
+    cout << i << endl;
+}
+
+return 0;
+}
