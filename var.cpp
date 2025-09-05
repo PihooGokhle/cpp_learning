c4e7c7cb8dcac4647402ad071fde53dd529fa5b6
PihooGokhle <pihoogokhle@gmail.com>
2025-09-05 20:37:36 +0530

    add var.cpp and vector.cpp files

---
+++ var.cpp
@@ -0,0 +1,12 @@
+# include<iostream>
+using namespace std;
+
+int main() {
+
+    int age = 19;
+    bool isage = false;
+    double price = 20.42;
+    cout << age << endl << isage << endl << price << endl;
+
+    return 0;
+}
---
+++ vector.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    vector<int> vec(4, 5);
+
+
+    cout << vec[0] << endl;
+    cout << vec[1] << endl;
+    cout << vec[2] << endl;
+    cout << vec[3] << endl;
+
+    return 0;
+}
